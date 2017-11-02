9bcd8d6d7de31dbaf4ec5f270247ad068c18134b
PullingStrings <drammatik@gmail.com>
2017-11-02 18:23:45 +0000

    Fixing linting error in user model

--- models/user.js
+++ models/user.js
@@ -81,4 +81,4 @@ userSchema.pre('remove', function removeImage(next) {
 //   return bcrypt.compareSync(password, this.password);
 // };
 
-module.exports = mongoose.model('User', userSchema, );
+module.exports = mongoose.model('User', userSchema);
